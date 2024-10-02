c8bc83457d1c0aa253b60f5551749597d7f4c0a5
SanketBhandarii <bhandarisanketp@gmail.com>
2024-10-02 16:22:53 +0530

    Updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://crick-zone.vercel.app"],
     credentials: true,
   })
 );
