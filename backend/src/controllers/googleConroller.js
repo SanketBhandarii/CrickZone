7a600950dda1ab0d975069d8e7c2d69643353dc5
SanketBhandarii <bhandarisanketp@gmail.com>
2024-10-02 18:43:36 +0530

    Updates

--- backend/src/controllers/googleConroller.js
+++ backend/src/controllers/googleConroller.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
+import { randomUUID } from "crypto";
 
 const generateTokenAndRespond = (user, res) => {
   const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, {
@@ -14,29 +15,37 @@ const generateTokenAndRespond = (user, res) => {
       secure: true,
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     })
-    .json({ msg: "Login Successful" });
+    .json({ 
+      msg: "Login Successful");
 };
 
 export const googleLogin = async (req, res) => {
   const { username, googleEmail } = req.body;
 
-  if (!username && !googleEmail) {
+  if (!username || !googleEmail) {
     return res.status(400).json({ msg: "Credentials are missing" });
   }
 
-  let user = await User.findOne({
-    $or: [{ username }, { email: googleEmail }],
-  });
+  try {
+    let user = await User.findOne({
+      $or: [{ username }, { email: googleEmail }],
+    });
 
-  if (user) {
+    if (user) {
+      return generateTokenAndRespond(user, res);
+    }
+
+    // Create a new user
+    user = await User.create({
+      username,
+      email: googleEmail,
+      password: randomUUID(),
+      verified: true,
+    });
+
     return generateTokenAndRespond(user, res);
+  } catch (error) {
+    console.error("Error during Google login:", error);
+    return res.status(500).json({ msg: "Internal server error" });
   }
-
-  user = await User.create({
-    username,
-    email: googleEmail,
-    verified: true,
-  });
-
-  generateTokenAndRespond(user, res);
 };
