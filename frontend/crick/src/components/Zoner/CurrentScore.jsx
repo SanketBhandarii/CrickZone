2cfccf75f35d143aaabf013914793274ef5cc68c
SanketBhandarii <bhandarisanketp@gmail.com>
2024-11-12 19:44:56 +0530

    Target Updated

--- frontend/crick/src/components/Zoner/CurrentScore.jsx
+++ frontend/crick/src/components/Zoner/CurrentScore.jsx
@@ -9,7 +9,7 @@ import { Context } from '../../store/Context'
       <h1><span className="text-sky-600">{inning}</span> Is Batting 🏏</h1>
         Current Score : {run} runs and {wicket} wickets
         {match == 2 ? (
-            <h1 className='text-sky-600'>Target : {parseInt(localStorage.getItem("t1run")) + 1}</h1>
+            <h1 className='text-sky-600 bg-yellow-300 rounded-lg'>Target : {parseInt(localStorage.getItem("t1run")) + 1}</h1>
           ) : null}
      </div>
    )
