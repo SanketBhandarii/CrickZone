5e843af4eea4d4bd3310cccc0977788f14772cb7
SanketBhandarii <bhandarisanketp@gmail.com>
2024-11-13 22:07:43 +0530

    Port Updation

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5173",
+      "http://localhost:5174",
       "https://crick-zone.vercel.app",
     ],
     credentials: true,
