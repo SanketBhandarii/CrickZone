977f04f7435e560d37183b0fce692bb017bf747c
SanketBhandarii <bhandarisanketp@gmail.com>
2024-12-14 11:20:47 +0530

    Updates

--- frontend/crick/src/components/landingPage/LandingPage.jsx
+++ frontend/crick/src/components/landingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -18,22 +19,37 @@ function LandingPage() {
           <i className="fa-brands fa-github text-3xl cursor-pointer"></i>
         </a>
       </nav>
-      <div className="h-auto flex flex-col justify-center items-center text-center py-20 mx-9">
-        <h2 className="text-4xl font-bold text-white bg-clip-text text-transparent mb-4">
-          Revolutionizing Cricket Score Tracking
-        </h2>
-        <p className="text-lg mb-6 max-w-xl text-slate-400">
-          CrickZone simplifies cricket scoring with real-time updates, seamless
-          management, and an effort from our side to ensure accuracy, fun, and
-          fairness for all
-        </p>
-        <button
-          className="px-8 py-3 text-lg font-bold bg-sky-600 text-white rounded-full transition-all duration-300 hover:bg-sky-700 hover:shadow-lg"
-          onClick={handleNavigate}
-        >
-          Get Started
-        </button>
-      </div>
+      <motion.div
+        className="card"
+        initial={{
+          opacity: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+          transition: {
+            duration: 3,
+          },  
+        }}
+        viewport={{ once: true }}
+      >
+        <div className="h-auto flex flex-col justify-center items-center text-center py-20 mx-9">
+          <h2 className="text-4xl font-bold text-white bg-clip-text text-transparent mb-4">
+            Revolutionizing Cricket Score Tracking
+          </h2>
+          <p className="text-lg mb-6 max-w-xl text-slate-400">
+            CrickZone simplifies cricket scoring with real-time updates,
+            seamless management, and an effort from our side to ensure accuracy,
+            fun, and fairness for all
+          </p>
+          <button
+            className="px-8 py-3 text-lg font-bold bg-sky-600 text-white rounded-full transition-all duration-300 hover:bg-sky-700 hover:shadow-lg"
+            onClick={handleNavigate}
+          >
+            Get Started
+          </button>
+        </div>
+      </motion.div>
     </div>
   );
 }
