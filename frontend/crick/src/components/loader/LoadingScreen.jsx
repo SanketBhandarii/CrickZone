447331c32e1b4909fad0925c0fdd564bf0a5aaca
SanketBhandarii <bhandarisanketp@gmail.com>
2025-06-19 22:40:42 +0530

    Response glitches solved

--- frontend/crick/src/components/loader/LoadingScreen.jsx
+++ frontend/crick/src/components/loader/LoadingScreen.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
 import Intro from "../Zoner/Intro";
+import useAxios from "@/api/axiosInstance";
 
 const LoadingScreen = ({ children }) => {
   const [loading, setLoading] = useState(true);
+  const [{ data }, execute] = useAxios(
+    {
+      method: "GET",
+      url: `/api/home`,
+      withCredentials: true,
+    },
+    { manual: true }
+  );
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
+    const checkAuth = async () => {
+      try {
+        const res = await execute();
+        if (res) {
+          setLoading(false);
+        }
+      } catch (err) {
+        return;
+      }
+    };
+    checkAuth();
   }, []);
 
-  if (loading) return <Intro />;
-
-  return children;
+  return loading ? <Intro /> : children;
 };
 
 export default LoadingScreen;
